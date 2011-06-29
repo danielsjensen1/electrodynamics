81c56afafebeb02a96c08d27280e927a512e9d16
Daniel Jensen <jensend@dino-laptop.home>
2011-06-29 06:53:59 -0400

    Removed homework files and renamed folders.

---
+++ src/efield/coulomb.py
@@ -0,0 +1,20 @@
+from numpy import isnan, mgrid, pi, sqrt
+from enthought.mayavi import mlab
+
+
+def efield(sources, grid):
+#~ def efield():
+    for source in sources:
+        position, charge = source
+        mlab.points3d([0e0], [0e0], [0e0], colormap="Reds", scale_factor=.25)
+        X, Y, Z = mgrid[-5:5:10j, -5:5:10j, -5:5:10j]
+        rminus3half = (X*X + Y*Y + Z*Z)**(-1.5)
+        rminus3half[isnan(rminus3half)] = 0
+        Ex, Ey, Ez = rminus3half * (X, Y, Z)
+    #  Run isnan on the data after adding all of the field contributions
+    mlab.quiver3d(X, Y, Z, Ex, Ey, Ez, line_width=2, scale_factor=2)
+    mlab.show()
+    
+if __name__ == '__main__':
+    sources = ((array([0,0,0]), q=1))
+    efield()
